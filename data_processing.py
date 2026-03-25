8ec932f67cbd0280d8c596614b3117291b3ba5b3
AdamAbdulkareeem <dmabdulkareem@gmail.com>
2026-03-25 21:34:35 +0100

    refactor: Changed the structure of the file read and write

--- data_processing.py
+++ data_processing.py
@@ -6,19 +6,14 @@ for filename in os.listdir(data_folder):
     if filename.endswith(".csv"):
         file_path = os.path.join(data_folder, filename)
 
-        with open(file_path, mode="r") as csv_file:
-            csv_reader = csv.DictReader(csv_file)
-            heading = csv_reader.fieldnames
-            for row in csv_reader:
-                with open("processed_data.csv", "a") as file:
-                    fieldnames = ["sales", heading[3], heading[4]]
-                    csv_writer = csv.DictWriter(file, fieldnames=fieldnames)
-                    csv_writer.writeheader()
+        with open("processed_data.csv", "w") as file:
+            csv_writer = csv.writer(file)
+            header = ["sales", "date", "region"]
+            csv_writer.writerow(header)
 
-                    if row["product"] == "pink morsel":
-                        morsel_sales = float(row["price"].replace("$", "")) * float(row["quantity"].replace("$", ""))
-                        csv_writer.writerow({
-                            "sales": morsel_sales,
-                            "date": row["date"],
-                            "region": row["region"]
-                        })
+            with open(file_path, mode="r") as csv_file:
+                csv_reader = csv.DictReader(csv_file)
+                for row in csv_reader:
+                        if row["product"] == "pink morsel":
+                            morsel_sales = float(row["price"][1:]) * float(row["quantity"])
+                            csv_writer.writerow([morsel_sales, row["date"], row["region"]])
